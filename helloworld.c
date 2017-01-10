10c575e2a940b24e87fec361f9efa316a1276ccb
prasanthi <prasanthi@savarinetworks.com>
2017-01-10 12:35:13 +0530

    helloworld merged

--- helloworld.c
+++ helloworld.c
@@ -1,30 +1,16 @@
 #include <stdio.h> 
 int main(void) 
-{ 
-<<<<<<< HEAD
-<<<<<<< HEAD
+    {  
+
 	printf("Hello! This is Prasanthi\n\n"); 
-<<<<<<< HEAD
-<<<<<<< HEAD
 	printf("Hello! Savari.., This is Manasa\n");
-       
-=======
-
-	Printf("Hi all... Am Jashitha\n\n"); 
-=======
-
+        printf("Hi all... Am Jashitha\n\n"); 
         printf("Hello This is Aswani\n\n"); 
->>>>>>> ed95bb0d139ff937fc94a8b74cf3af44a0cc2da1
-=======
         printf("hello! iam pallavi\n\n");       
->>>>>>> adb0f3f4ffebdd5c3533eb027e372c5e16a73a33
+        printf("Hello! This is Sravani\n\n");
+            return(0);
 
->>>>>>> 7200589a362c650db56c9486cd3ff017bc2c65e6
-=======
-       printf("Hello! This is Sravani\n\n");
-       return(0);
 
->>>>>>> c7ee0c22b669fdc1daa35287f69b516c7d5408f1
-}
+     }
 
 
